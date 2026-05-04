133282e3949cb66ae6bc9bb8a8bcc7b741226780
soro bedio <sorobedio@soros-MacBook-Pro.local>
2026-05-05 06:51:49 +0900

    updating

--- run_pretrain_fineweb_hyperclone_cont.sh
+++ run_pretrain_fineweb_hyperclone_cont.sh
@@ -54,7 +54,7 @@ torchrun \
   --logging_dir "./logs" \
   --report_to "wandb" \
   \
-  --tokens_per_checkpoint 100000000 \
+  --tokens_per_checkpoint 20000000 \
   --eval_tokens_interval 1000000 \
   \
   --eval_strategy "no" \
